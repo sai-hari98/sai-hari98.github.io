4732a417dfe55dda07eb2bfac6065a9e871fbf98
Sairam Harikrishnan <sai_hari98@hotmail.com>
2023-12-21 19:36:00 -0600

    removed layout animation

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ const StyledContent = styled.div`
 `;
 
 const Layout = ({ children, location }) => {
-  const isHome = location.pathname === '/';
+  const isHome = false; //modified to remove animation
   const [isLoading, setIsLoading] = useState(isHome);
 
   // Sets target="_blank" rel="noopener noreferrer" on external links
